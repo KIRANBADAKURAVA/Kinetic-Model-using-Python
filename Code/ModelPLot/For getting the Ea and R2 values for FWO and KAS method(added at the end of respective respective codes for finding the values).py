b8ecc61ae733d47af5b14daf233ff7d55310809c
Kluxuva <165916318+Kluxuva@users.noreply.github.com>
2024-08-21 22:21:32 +0530

    Update For getting the Ea and R2 values for FWO and KAS method(added at the end of respective respective codes for finding the values).py

--- Code/ModelPLot/For getting the Ea and R2 values for FWO and KAS method(added at the end of respective respective codes for finding the values).py
+++ Code/ModelPLot/For getting the Ea and R2 values for FWO and KAS method(added at the end of respective respective codes for finding the values).py
@@ -11,3 +11,11 @@ for alpha in alpha_values:
         'R²': r2_values[alpha],
         'Ea (J/mol)': Ea_values[alpha]
     })
+# Convert to DataFrame
+output_df = pd.DataFrame(output_data)
+
+# Save to Excel
+output_file_path = r'C:\Users\Pranav\Desktop\Final H valuessssss\alpha_values_results1CRD1.xlsx'
+output_df.to_excel(output_file_path, index=False)
+
+print(f"Data saved to '{output_file_path}'")
